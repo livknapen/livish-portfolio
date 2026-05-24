e5da1a58d1962b12644ff19fc0af7cf87b7e48cf
livknapen <59344183+livknapen@users.noreply.github.com>
2026-05-24 12:34:00 +0200

    startup

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from "../pages/styles/Home.module.css";
+
+
+export default function Home() {
+  return (
+    <>
+<main className={styles.main}>
+    <div>
+      <h1>Hallo wereld</h1>
+    </div>
+      </main>
+       </>
+  );
+}
